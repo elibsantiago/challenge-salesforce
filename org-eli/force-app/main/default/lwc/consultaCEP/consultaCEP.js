a7018be2cd1c9d933010675ecf0c842c03205247
Elibergh Santiago <elibergh@gmail.com>
2022-07-31 12:28:36 -0400

    salvando na conta

--- org-eli/force-app/main/default/lwc/consultaCEP/consultaCEP.js
+++ org-eli/force-app/main/default/lwc/consultaCEP/consultaCEP.js
@@ -1,5 +1,62 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api} from 'lwc';
+import consultarCEP from '@salesforce/apex/consultaCEPController.consultaCEP';
+import saveAddress from '@salesforce/apex/consultaCEPController.saveAddress';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 
+export default class ConsultaCEP extends LightningElement {
 
-export default class ConsultaCEP extends LightningElement {}
+    @api recordId
+
+    endereco = {};
+    cep = '';
+
+    ativaConsulta() {
+        consultarCEP({cepDigitado : this.cep})
+            .then(result => {
+                this.endereco = result;
+                console.log(JSON.stringify(result));
+                console.log(this.cep)
+                console.log(this.recordId)
+                //console.log(result.logradouro)
+            })
+            .catch(error => {
+                this.error = error;
+                
+            });
+    }
+
+    handleCepSearch(event) {
+        this.cep  = event.target.value
+    }
+
+
+    async handleSave (event) {
+        let parametro = {
+            recordId : this.recordId,
+           // address : JSON.stringify(this.endereco)
+           endereco : {...this.endereco}
+           
+        //     "cep"  : this.endereco.cep,
+        //     "logradouro" : this.endereco.logradouro,
+        //     "complemento"  : this.endereco.complemento,
+        //     "bairro" : this.endereco.bairro,
+        //     "localidade" : this.endereco.localidade,
+        //     "uf" : this.endereco.uf,
+        //     "ibge" : this.endereco.ibge,
+        //     "gia" : this.endereco.gia,
+        //     "ddd" : this.endereco.ddd,
+        //     "siafi" : this.endereco.siafi
+        //    }
+        }
+        console.log(JSON.stringify(parametro))
+      await saveAddress(parametro) 
+      .then(result => {
+        console.log(result)
+        this.dispatchEvent(new ShowToastEvent({ title: 'Efsdaf!', message: "Escolha uma data futura.", variant: 'success' }));
+      })
+      .catch(error => {
+        this.dispatchEvent(new ShowToastEvent({ title: 'Erro!', message: error, variant: 'error' }));
+      })
+    }
+}
